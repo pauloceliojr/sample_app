7a4c27e2c0cccabf13927560a121b80c469db2b6
Paulo Celio Junior <pauloceliojr@cnmp.mp.br>
2014-04-28 20:30:42 -0300

    Add user following.

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -7,4 +7,12 @@ class Micropost < ActiveRecord::Base
 
   validates :content, presence: true, length: { maximum: 140 }
   validates :user_id, presence: true
+
+  def self.from_users_followed_by(user)
+    # followed_user_ids = user.followed_user_ids
+    # where('user_id = ? or user_id in (?)', user, followed_user_ids)
+
+    followed_user_ids = 'SELECT followed_id FROM relationships WHERE follower_id = :user_id'
+    where("user_id = :user_id or user_id in (#{followed_user_ids})", user_id: user)
+  end
 end
